8b500a9f36c69b17b4e19ccb2a88298e3a8291d1
Link4Electronics <link4electronics@gmail.com>
2026-04-05 19:54:27 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,20 +3,25 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q dhewm3-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/scalable/apps/org.dhewm3.Dhewm3.svg
-export DESKTOP=/usr/share/applications/org.dhewm3.Dhewm3.desktop
+export ICON=https://raw.githubusercontent.com/dhewm/dhewm3/455b88e8dff2be822f08eb498f51b383e851fa38/dist/linux/share/icons/hicolor/scalable/apps/org.dhewm3.Dhewm3.svg
+export DESKTOP=https://raw.githubusercontent.com/dhewm/dhewm3/refs/heads/master/dist/linux/share/applications/org.dhewm3.Dhewm3.desktop
 export STARTUPWMCLASS=dhewm3
 export DEPLOY_OPENGL=1
+export DEPLOY_PULSE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/dhewm3 /usr/bin/dhewm3ded /usr/lib/dhewm3/*
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/* /usr/lib/libopenal.so*
+echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
+# this app has problems with other locales breaking physics
+echo 'LC_ALL=C.UTF-8' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --simple-test ./dist/*.AppImage
