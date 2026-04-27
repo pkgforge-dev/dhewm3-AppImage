ec40aa34e28004649c3ea1e676699db3359b3cb4
Link4Electronics <link4electronics@gmail.com>
2026-04-27 07:03:58 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,12 +12,13 @@ export ICON=/usr/share/icons/hicolor/scalable/apps/org.dhewm3.Dhewm3.svg
 export DESKTOP=/usr/share/applications/org.dhewm3.Dhewm3.desktop
 export STARTUPWMCLASS=dhewm3
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
+export DEPLOY_PULSE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/dhewm3 /usr/bin/dhewm3ded /usr/lib/dhewm3/*
 
 # Additional changes can be done in between here
+echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
