5eb1c4cf12da8fc3a0bc39bd0091067b3a10d2ac
Link4Electronics <link4electronics@gmail.com>
2026-09-01 21:34:38 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,10 +16,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package dhewm3-git
-
-# If the application needs to be manually built that has to be done down here
 echo "Building dhewm3..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/dhewm/dhewm3"
